dbe893f659c17a32261bfbc1ad2f9d72147af7bf
dbarberisbestani89 <152974620+dbarberisbestani89@users.noreply.github.com>
2024-01-03 12:31:00 -0300

    Delete 10-json directory

--- 10-json/datosBici.js
+++
@@ -1,7 +0,0 @@
-let fs = require("fs");
-
-let rutaArchivo = "./bicicletas.json";
-let bicicletasJSON = fs.readFileSync(rutaArchivo, "utf-8");
-let bicicletasArray = JSON.parse(bicicletasJSON);
-
-module.exports = bicicletasArray;
